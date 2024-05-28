79b5f973f54572abd8ceb52addeee049ce35091a
Holden.C <617240374@qq.com>
2024-05-28 14:02:48 +0800

    首次提交

--- numpy_handler/db_handler.py
+++ numpy_handler/db_handler.py
@@ -9,15 +9,15 @@ logger = logging.getLogger()
 class NumpyReadDb:
 
     def __init__(self):
-        self.db = db_col
-        if self.db.db is not None:
+        self.db_cl = db_col
+        if self.db_cl.db is not None:
             self.init_query()
 
 
     def init_query(self):
         # todo此处可以对处理数据进行进一步query筛选
-        with self.db:
-            result = self.db.run_query()
+        with self.db_cl:
+            result = self.db_cl.run_query()
             for i, history in enumerate(result):
                 print('{} : {}'.format(i, history))
 
