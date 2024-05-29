671c25d9db32106a0e7357f7f1c737bbdc5c9a50
Holden.C <617240374@qq.com>
2024-05-29 17:03:41 +0800

    首次提交

--- numpy_handler/db_handler.py
+++ numpy_handler/db_handler.py
@@ -128,6 +128,7 @@ class NumpyReadDb:
                 page += 1
                 self.write_excel(nps, str(page))
                 nps = nps.insert(0, self.title)
+                print(nps[0])
                 np_apply = get_analysis(AvgStrategy(), np.array(nps))
                 self.write_excel(np_apply, str(page)+'_avg')
                 print('已完成处理数据第{}页'.format(page))
