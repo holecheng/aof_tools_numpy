644450a1912a8e749e5553588b1e78d0a6188fff
Holden.C <617240374@qq.com>
2024-05-30 18:25:33 +0800

    首次提交

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import numpy as np
 
 npd = [[1, np.nan, 5], [2, 3, 1], [2, 3, 7], [2, 8, np.nan]]
 npd = np.array(npd)
-
-print()
+a = npd[1].tolist()
+for i in npd[1]:
+    print(i, type(i))
+    print(i == 1)
 
