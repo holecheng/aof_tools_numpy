e37874cac9b843b3f2e81ea212946f08585978b2
Holden.C <617240374@qq.com>
2024-05-31 15:30:21 +0800

    首次提交

--- db/db_loader.py
+++ db/db_loader.py
@@ -65,7 +65,7 @@ class DBLoader:
         dic_zero = dict.fromkeys(list(set(COLUMNS) - pid_columns), 0)
         dic_one = dict.fromkeys(pid_columns, 1)
         dic_zero.update(dic_one)
-        print(dic_zero)
+        print(dic_zero, type(dic_zero))
         result = self.db.find(self.query, {'evoutcome': 0, 'turn': 0, 'handNumber': 0, 'reqid': 0, 'winners': 0, 'nash_range': 0,
                                   'blindLevel': 0, 'version': 0, 'river': 0, 'leagueName': 0, 'command': 0,
                                   'players': 1, 'heroIndex': 1}
