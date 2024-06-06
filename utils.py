c6eb2ec253dfb9f6a17c7900f13ce3802bfe3b12
Holden.C <617240374@qq.com>
2024-06-06 16:22:00 +0800

    [add]脚本

--- utils.py
+++ utils.py
@@ -59,7 +59,6 @@ def resize_insurance(row_key):
 
 def get_group_key(group_key, row_dic=None):
     allowance = config.get_args('allowance')
-    print(allowance)
     if group_key == 'total':
         return allowance, group_key
     if allowance:
