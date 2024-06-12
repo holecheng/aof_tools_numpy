e34e1984a8b85facc9b71253285c22fabc750cac
Holden.C <617240374@qq.com>
2024-06-12 14:46:04 +0800

    [add]脚本

--- group/blinds_group.py
+++ group/blinds_group.py
@@ -39,6 +39,7 @@ class Blinds(Base):
         row_dic = other.row_dic
         self.counts += 1
         self.covert(row_dic)
+        print('此刻self{}'.format(self.sum_ev_player))
         return self
 
     def covert(self, row_dic, types='add'):
@@ -66,11 +67,7 @@ class Blinds(Base):
         if not counts:
             counts = self.counts
         if types == 'add':
-            if suffix == 'ev_player':
-                print('第一轮初始值{}'.format(getattr(self, 'avg_' + suffix)))
-                print('本轮需要处理的值%s' % (row_dic[suffix]))
-                print('相加后的值{}'.format(getattr(self, 'avg_' + suffix) + float(row_dic[suffix])))
-            setattr(self, 'sum_' + suffix, getattr(self, 'avg_' + suffix) + float(row_dic[suffix]))
+            setattr(self, 'sum_' + suffix, getattr(self, 'sum_' + suffix) + float(row_dic[suffix]))
             setattr(self, 'avg_' + suffix, self.avg_get(getattr(self, 'sum_' + suffix), counts))
         else:
             setattr(self, 'avg_'+suffix, float(row_dic[suffix]))
