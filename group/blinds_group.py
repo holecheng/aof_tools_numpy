73171c88cb31451eb1ec86b0338c6f87b9a6ce7c
Holden.C <617240374@qq.com>
2024-06-12 14:38:13 +0800

    [add]脚本

--- group/blinds_group.py
+++ group/blinds_group.py
@@ -68,7 +68,7 @@ class Blinds(Base):
         if types == 'add':
             if suffix == 'ev_player':
                 print('本轮需要处理的值%s' % (row_dic[suffix]))
-                print('相加后的值'.format(getattr(self, 'avg_' + suffix) + float(row_dic[suffix])))
+                print('相加后的值{}'.format(getattr(self, 'avg_' + suffix) + float(row_dic[suffix])))
             setattr(self, 'sum_' + suffix, getattr(self, 'avg_' + suffix) + float(row_dic[suffix]))
             setattr(self, 'avg_' + suffix, self.avg_get(getattr(self, 'sum_' + suffix), counts))
         else:
