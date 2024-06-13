e2e7fdbabc425b933077f0d57d2a7e01cf04a0b7
Holden.C <617240374@qq.com>
2024-06-13 18:10:42 +0800

    [add]玩家场次

--- group/blinds_group.py
+++ group/blinds_group.py
@@ -11,9 +11,6 @@ class Blinds(Base):
                  'avg_flop_ev',  # 第一轮三张牌之后的期望（均值）
                  'avg_turn_ev',  # turn牌发出来的期望（均值）
                  'avg_ai_stack',  # 场内ai所持砝码
-                 'avg_ai_count',  # 场内ai人数
-                 'avg_player_count',  # 场内玩家人数
-                 'avg_compare_player',  # 玩家人数比
                  'avg_compare_stack',  # 场内ai所持砝码对比对方所持砝码
                  'counts',  # 总对局数-同一局按AI出现次数计
                  'turn_count',  # 分发到第一张牌的总场次  turn-counts 因为发了turn一定会继续下去所以计算一个
@@ -22,10 +19,7 @@ class Blinds(Base):
                  'sum_flop_ev',
                  'sum_turn_ev',
                  'sum_ai_stack',
-                 'sum_ai_count',
-                 'sum_player_count',
                  'sum_compare_stack',
-                 'sum_compare_player',
                  'row_dic',
                  )
 
@@ -57,9 +51,6 @@ class Blinds(Base):
             self.add_or_init('outcome_player', row_dic)
             self.add_or_init('ai_stack', row_dic)
             self.add_or_init('compare_stack', row_dic)
-            self.add_or_init('ai_count', row_dic)
-            self.add_or_init('player_count', row_dic)
-            self.add_or_init('compare_player', row_dic)
         else:
             if row_dic['is_turn']:
                 self.turn_count += 1
@@ -69,9 +60,6 @@ class Blinds(Base):
             self.add_or_init('outcome_player', row_dic, types='init')
             self.add_or_init('ai_stack', row_dic, types='init')
             self.add_or_init('compare_stack', row_dic, types='init')
-            self.add_or_init('ai_count', row_dic, types='init')
-            self.add_or_init('player_count', row_dic, types='init')
-            self.add_or_init('compare_player', row_dic, types='init')
         setattr(self, 'diff_ev_outcome', self.avg_outcome_player - self.avg_ev_player)
 
     def add_or_init(self, suffix, row_dic, types='add', counts=None):
