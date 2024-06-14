818c6c08c41d24894a29907f62ce3ea6d22f6b78
Holden.C <617240374@qq.com>
2024-06-14 17:17:30 +0800

    [add]玩家场次

--- lib/compare.py
+++ lib/compare.py
@@ -2,32 +2,27 @@ huan = []
 cheng = []
 
 
-with open('../output/2024-04-01,2024-04-02hand_detail.csv', 'r') as f:
+with open('4-01-4-30.csv', 'r', encoding='utf-8') as f:
     for i in f.readlines():
-        q, p = i.strip().split(',')
-        cheng.append((q, p))
+        if i.split(',')[-1].strip() != '2024-04-01' and i.split(',')[-1].strip() != 'timestamp':
+            break
+        cheng.append(i)
 
-with open('game.csv', 'r') as f:
+with open('all_detail.csv', 'r', encoding='utf-8') as f:
     for i in f.readlines():
-        p, q = i.replace('\n', '').strip().split(',')
-        huan.append((q, p))
+        if i.split(',')[-1].strip() != '2024-04-01' and i.split(',')[-1].strip() != 'timestamp':
+            break
+        huan.append(i)
 
 diff = []
-for i in huan:
-    if i not in cheng:
+for i in cheng:
+    if i not in huan:
         diff.append(i)
 
 print(len(diff))
+
 for i in diff:
     print(i)
 
-diff2 = []
-for i in cheng:
-    if i not in huan:
-        diff2.append(i)
-
-print(len(diff2))
-for i in diff2:
-    print(i)
 
 
--- numpy_handler/db_handler.py
+++ numpy_handler/db_handler.py
@@ -72,6 +72,8 @@ def init_query():
             else:
                 compare_stack = ai_stack / (sum([int(i.get('stack')) / ante for i in players]) - ai_stack)
             for hero_index, player in enumerate(players):
+                if str(player.get('pId')) == '5797530797' and str(i.get('handNumber')) == '101651575-27':
+                    print(i)
                 if config.get_args('player') and str(config.get_args('player')) != player.get('pId'):
                     continue
                 row_dic = collections.defaultdict(str)
