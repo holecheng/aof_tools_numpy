587d9226363cd97d97706fcb03604e6f66748f40
Holden.C <617240374@qq.com>
2024-06-18 10:42:52 +0800

    [add]玩家场次

--- numpy_handler/db_handler.py
+++ numpy_handler/db_handler.py
@@ -36,8 +36,8 @@ def init_query():
         sum_ev = 0
         sum_outcome = 0
         for i in result:
-            if str(i.get('handNumber')) == '101652141-60':
-                print(i)
+            # if str(i.get('handNumber')) == '101652141-60':
+            #     print(i)
             is_success, _ = RowHand().convert(i)
             if not is_success:
                 continue
@@ -200,6 +200,7 @@ class NumpyReadDb:
         title.remove('row_dic')
         ans = [title]
         for _, v in self.group_dic.items():
+            print(v)
             ans.append([round(getattr(v, i), 5) if isinstance(getattr(v, i), float)
                         else getattr(v, i) for i in title])
         if self.group:
