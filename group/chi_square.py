7b0c4ae95a07de5b8bcf2b579b3c72c98afb270a
Holden.C <617240374@qq.com>
2024-06-18 13:56:18 +0800

    [add]玩家场次

--- group/chi_square.py
+++ group/chi_square.py
@@ -30,7 +30,7 @@ class ChiSquareCheck:
         if self.total:
             return self  # 去除total干扰
         pid_case = row_dic.get('pid_case')
-        print(pid_case)
+        print(type(pid_case))
         pid_case = json.loads(pid_case)
         if not pid_case:
             return self  # 没有数据不计入
