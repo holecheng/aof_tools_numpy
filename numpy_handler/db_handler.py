dc5618f75b7914e81310af03fda90abbc90c69b7
Holden.C <617240374@qq.com>
2024-06-19 15:16:38 +0800

    [add]去掉csv存储

--- numpy_handler/db_handler.py
+++ numpy_handler/db_handler.py
@@ -261,7 +261,6 @@ class NumpyReadDb:
                         continue
                     unique[hand.handno] = 1
                     players = i.get('players')
-
                     ai_count = sum(1 if i.get('pId') in db_col.pid_set else 0 for i in players)
                     if ai_count == len(players):
                         continue
