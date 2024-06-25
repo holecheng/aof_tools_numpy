f679f4c255d3c672e9a2f1afa82e54a314a80f2e
Holden.C <617240374@qq.com>
2024-06-25 18:06:24 +0800

    [fix]增加平均值total

--- lib/variance.py
+++ lib/variance.py
@@ -10,15 +10,11 @@ def run():
     parser.add_argument('--f-name', type=str, nargs='?', help='文件名称', default='')
     args = parser.parse_args()
     df = pd.read_csv(str(os.path.join(BASE, args.f_name)))
-    total = df[df['group_key'] == 'total'].iloc[:, [2, 3]]
+    total = df[df['group_key'] == 'total'].loc[:, ['avg_ev_player', 'avg_outcome_player']]
     df = df[df['group_key'] != 'total']
     df = df.drop(columns=['diff_ev_outcome', 'sum_outcome_player', 'sum_ev_player', 'total'], axis=1)
     df.index = [i for i in range(df.shape[0])]
     av = df.loc[:, ['avg_ev_player', 'avg_outcome_player']]
-    # print('------------------------------------')
-    # print(total, av.shape[0])
-    # print('-------------------------------------')
-    # print(av.sub(total))
     dc = []
     for i, r in av.iterrows():
         dc.append([(r.avg_ev_player - total.avg_ev_player[1]) ** 2,
@@ -26,6 +22,7 @@ def run():
     dc = pd.DataFrame(dc)
     dc.columns = ['var_ev_player', 'var_outcome_player']
     ans = pd.concat([dc, df], axis=1)
+    ans = pd.concat([ans, total], axis=0)
     ans.to_csv('write_csv/{}'.format(args.f_name))
 
 
